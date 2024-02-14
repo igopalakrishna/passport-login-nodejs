8c11e58d49fc99446c9b1cd19082e5e2f6f64b0a
Gopala Krishna <79021861+igopalakrishna@users.noreply.github.com>
2024-02-14 21:09:39 +0530

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -6,49 +6,38 @@ const passport = require('passport');
 const User = require('../models/User');
 const { forwardAuthenticated } = require('../config/auth');
 
-// Login Page
-router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+//Login Page 
+router.get('/login', (req,res) => res.render('login'));
 
-// Register Page
-router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+//Register Page 
+router.get('/register', (req,res) => res.render('register'));
 
 // Register
 router.post('/register', (req, res) => {
-  const { name, email, password, password2 } = req.body;
-  let errors = [];
-
-  if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
-  }
-
-  if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
-  }
-
-  if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
-  }
-
-  if (errors.length > 0) {
-    res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      password2
-    });
-  } else {
-    User.findOne({ email: email }).then(user => {
-      if (user) {
-        errors.push({ msg: 'Email already exists' });
-        res.render('register', {
-          errors,
-          name,
-          email,
-          password,
-          password2
-        });
-      } else {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+  
+    if (!name || !email || !password || !password2) {
+      errors.push({ msg: 'Please enter all fields' });
+    }
+  
+    if (password != password2) {
+      errors.push({ msg: 'Passwords do not match' });
+    }
+  
+    if (password.length < 6) {
+      errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+  
+    if (errors.length > 0) {
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+      });
+    } else {
         const newUser = new User({
           name,
           email,
@@ -73,8 +62,6 @@ router.post('/register', (req, res) => {
         });
       }
     });
-  }
-});
 
 // Login
 router.post('/login', (req, res, next) => {
@@ -87,9 +74,15 @@ router.post('/login', (req, res, next) => {
 
 // Logout
 router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  req.logout((err) => {
+    if (err) {
+      console.error(err);
+    }
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+  });
 });
 
+
 module.exports = router;
+
